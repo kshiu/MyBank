c038d7e19914666baffd2bf5f672e730b8edb7f6
karen.shiu <kshiu2@gmail.com>
2016-12-08 02:19:27 -0800

    Initial Commit

---
+++ MyBank/Account.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*multiple lines of comments
+ *
+*/
+
+//single line comment
+namespace MyBank
+{
+    public enum TypeOfAccounts
+    {
+        Checking,
+        Savings,
+    }
+    /// <summary>
+    /// This class describes a bank account
+    /// This is an xml comment style
+    /// </summary>
+    public class Account
+    {
+        #region Statics
+            private static int lastAccountNumber = 0;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Account number of the account
+        /// </summary>
+        public int AccountNumber { get; private set; }
+        /// <summary>
+        /// Email address of the user
+        /// </summary>
+        public string EmailAddress { get; set; }
+        public decimal Balance { get;
+            private set; }
+        public TypeOfAccounts AccountType { get; set; }  //only want 2 options to be given (Checking and Savings accounts)
+        #endregion
+
+        #region Constructors
+        public Account()
+        {
+            //lastAccountNumber += 1;
+            //AccountNumber = lastAcccountNumber; 
+            AccountNumber = ++lastAccountNumber;   //This is a post increment operator vs the preincrement of lastAccountNumber++
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Deposit money into your account
+        /// </summary>
+        /// <param name="amount">Amount to be deposited</param>
+        /// <returns>The updated balance</returns>
+        public decimal Deposit(decimal amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+
+        public decimal Withdraw(decimal amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            return Balance;
+        }
+        #endregion
+    }
+}
---
+++ MyBank/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*********************");
+            Console.WriteLine("Welcome to my bank!");
+            var choice = "-1";
+            while (choice != "0")
+            {
+                Console.WriteLine("Please select from the list of choices below: ");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Create Account");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3. Withdraw");
+                choice = Console.ReadLine(); //accepts input from user
+                switch (choice)  //switch tab tab 
+                {
+                    case "0":
+                        Console.WriteLine("Have a great day!");
+                        return;
+                    case "1":
+                        Console.Write("Email Address: ");
+                        var emailAddress = Console.ReadLine();
+                        //instantiating a class[a new Account]
+                        var checkingAccount = new Account();
+                        //no longer able to use this line bc you should not be able to set AccountNumber -> checkingAccount.AccountNumber = 123;
+                        checkingAccount.EmailAddress = emailAddress;
+                        checkingAccount.AccountType = TypeOfAccounts.Checking; //enum list of options
+                                                                               // checkingAccount.Deposit(1002.50M); //use 'M' to specify decimal. M stands for Money
+                        Console.WriteLine($"Account number: {checkingAccount.AccountNumber}, Balance: {checkingAccount.Balance:C}"); //C stands for currency
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            /* var savingsAccount = new Account();
+            savingsAccount.EmailAddress = "test2@test.com";
+            savingsAccount.AccountType = TypeOfAccounts.Savings;
+            savingsAccount.Deposit(200M);
+            Console.WriteLine($"Account number: {savingsAccount.AccountNumber}, Balance: {savingsAccount.Balance:C}");
+            */
+        }
+    }
+}
